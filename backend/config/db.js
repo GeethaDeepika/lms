a00ef1648e3a98824840ecc5e205bee66cbd69ff
GeethaDeepika <deepikakatragadda04@gmail.com>
2024-11-13 10:37:39 +0530

    till login

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,24 +1,8 @@
-// // config/db.js
-// const mongoose = require('mongoose');
-// require('dotenv').config();
-
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URI); // Remove deprecated options
-//         console.log('MongoDB connected');
-//     } catch (error) {
-//         console.error('Database connection failed', error);
-//         process.exit(1);
-//     }
-// };
-
-// module.exports = connectDB;
-
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB Connected');
   } catch (error) {
     console.error(error);
