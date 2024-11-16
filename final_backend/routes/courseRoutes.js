6935e2ea1637ac34a0183a295878095324b6872c
Venu Pulagam <notvenupulagam@gmail.com>
2024-11-17 00:28:43 +0530

    Complete course addition

--- final_backend/routes/courseRoutes.js
+++ final_backend/routes/courseRoutes.js
@@ -8,12 +8,10 @@ const AWS = require('aws-sdk');
 const storage = multer.memoryStorage(); 
 const upload = multer({ storage: storage });
 
-const aws_part_two = 'LsqsxXo3QYFVRBWmrUhL'
-
 // Configure AWS S3
 const s3 = new AWS.S3({
-    accessKeyId: process.env.accessKeyId,
-    secretAccessKey: process.env.secretAccessKey,
+    accessKeyId: '',
+    secretAccessKey: '',
     region: 'eu-north-1'
 });
 
